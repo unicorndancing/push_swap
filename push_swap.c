57509f3e836b54ea6f154e1ed83b13a67a16b4ad
unicorndancing <128758829+unicorndancing@users.noreply.github.com>
2024-04-03 16:38:37 +0200

    Add files via upload

--- push_swap.c
+++ push_swap.c
@@ -6,102 +6,87 @@
 /*   By: mlapique <mlapique@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 13:49:51 by mlapique          #+#    #+#             */
-/*   Updated: 2024/03/06 16:05:17 by mlapique         ###   ########.fr       */
+/*   Updated: 2024/04/03 15:48:58 by mlapique         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	verif_in_order(t_stack *stack_a)
+void	rotaterr(t_stack **stack_a, t_stack **stack_b, int i)
 {
-	t_stack	*temp;
-	t_stack	*temp2;
-
-	temp = stack_a;
-	temp2 = stack_a->next;
-	while (temp2)
-	{
-		if (temp->suposed_place >= temp2->suposed_place)
-			return (1);
-		temp = temp->next;
-		temp2 = temp2->next;
-	}
-	return (0);
-}
-
-void	rotaterr(t_stack **stack_a, t_stack **stack_b)
-{
-	int	max;
 	int	ndmax;
+	int	max;
 
-	ndmax = get_ndmax(stack_b);
 	max = get_max_suposed_place(stack_b);
+	ndmax = get_ndmax(stack_b);
 	while ((*stack_b)->next != NULL)
 	{
 		if ((*stack_b)->suposed_place == max)
 		{
 			do_pa(stack_a, stack_b);
+			if (ndmax == -1)
+				do_sa(stack_a);
 			break ;
 		}
-		else if ((*stack_b)->suposed_place == ndmax)
+		else if ((*stack_b)->suposed_place == ndmax && i != 0)
 		{
 			do_pa(stack_a, stack_b);
-			do_ra(stack_a);
-			break ;
+			ndmax = -1;
 		}
 		else
 			do_rrb(stack_b);
 	}
+	if ((*stack_b)->next == NULL)
+		do_pa(stack_a, stack_b);
 }
 
-void	rotater(t_stack **stack_a, t_stack **stack_b)
+void	rotater(t_stack **stack_a, t_stack **stack_b, int i)
 {
-	int	max;
 	int	ndmax;
 
 	ndmax = get_ndmax(stack_b);
-	max = get_max_suposed_place(stack_b);
 	while ((*stack_b)->next != NULL)
 	{
-		if ((*stack_b)->suposed_place == max)
+		if ((*stack_b)->suposed_place == get_max_suposed_place(stack_b))
 		{
 			do_pa(stack_a, stack_b);
+			if (ndmax == -1)
+				do_sa(stack_a);
 			break ;
 		}
-		else if ((*stack_b)->suposed_place == ndmax)
+		else if ((*stack_b)->suposed_place == ndmax && i != 0)
 		{
 			do_pa(stack_a, stack_b);
-			do_ra(stack_a);
-			break ;
+			ndmax = -1;
 		}
 		else
 			do_rb(stack_b);
 	}
+	if ((*stack_b)->next == NULL)
+		do_pa(stack_a, stack_b);
 }
 
 void	sort_little_by_little(t_stack **stack_a, t_stack **stack_b, int i)
 {
 	int	which;
-	// int ipo = 0;
 
-	if (i == 1)
-	{
-		while ((*stack_a)->next != NULL)
-			do_pb(stack_a, stack_b);
-		do_pb(stack_a, stack_b);
-	}
-	which = way_to_rotate (stack_a, stack_b);
+	which = way_to_rotate (stack_b);
 	if (which == 0)
-		rotater(stack_a, stack_b);
+		rotater(stack_a, stack_b, i);
 	else
-		rotaterr(stack_a, stack_b);
-	if (!(*stack_b)->next)
+		rotaterr(stack_a, stack_b, i);
+	if ((*stack_b))
 	{
-		// while (is_sorted(*stack_a, *stack_b) == 0 && ipo < 200)
-		// {
-		// 	do_rra(stack_a);
-		// 	ipo++;
-		// }
+		if ((*stack_b)->next == NULL)
+		{
+			if ((*stack_b)->suposed_place == 0)
+				do_pa(stack_a, stack_b);
+		}
+	}
+	if ((*stack_a)->next != NULL)
+	{
+		if ((*stack_a)->suposed_place > (*stack_a)->next->suposed_place)
+			do_sa(stack_a);
 	}
 	return ;
 }
@@ -109,23 +94,23 @@ void	sort_little_by_little(t_stack **stack_a, t_stack **stack_b, int i)
 int	push_swap(t_stack **stack_a, t_stack **stack_b)
 {
 	int	i;
-	int	last_index;
+	int	li;
 
-	last_index = get_last_index(*stack_a);
-	i = 1;
-	if (last_index == 3)
-		do_the_little_sort(stack_a, stack_b);
-	else if (last_index == 4)
-		do_the_less_little_sort(stack_a, stack_b);
-	else if (last_index == 5)
-		do_the_less_less_little_sort(stack_a, stack_b);
-	else
+	i = 0;
+	li = get_last_index(*stack_a);
+	while (i < li)
 	{
-		while (is_sorted(*stack_a, *stack_b) != 1 && i < 5000)
-		{
-			sort_little_by_little(stack_a, stack_b, i);
-			i++;
-		}
+		if (get_last_index(*stack_a) + get_last_index(*stack_b) > 250)
+			ipresort(stack_a, stack_b, (li / 17) + (i * (li / 17)), (li / 17));
+		else if (get_last_index(*stack_a) + get_last_index(*stack_b) > 10)
+			ipresort(stack_a, stack_b, (li / 10) + (i * (li / 10)), (li / 10));
+		else if (get_last_index(*stack_a) + get_last_index(*stack_b) < 10)
+			ipresort(stack_a, stack_b, (li / 2) + (i * (li / 10)), (li / 2));
+		i += 2;
 	}
+	if (stack_a)
+		do_pb(stack_a, stack_b);
+	while (is_sorted(*stack_a, *stack_b) != 1)
+		sort_little_by_little(stack_a, stack_b, i);
 	return (SUCCES);
 }
--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,13 +6,13 @@
 /*   By: mlapique <mlapique@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 14:59:58 by mlapique          #+#    #+#             */
-/*   Updated: 2024/03/06 16:10:10 by mlapique         ###   ########.fr       */
+/*   Updated: 2024/04/03 16:33:47 by mlapique         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	way_to_rotate(t_stack **stack_a, t_stack **stack_b)
+int	way_to_rotate(t_stack **stack_b)
 {
 	int		max;
 	int		ndmax;
@@ -21,22 +21,21 @@ int	way_to_rotate(t_stack **stack_a, t_stack **stack_b)
 	int		nb_instack;
 
 	tmp = *stack_b;
-	nb_instack = get_last_index(*stack_a) - 1;
+	nb_instack = get_last_index(*stack_b) - 1;
 	max = get_max_suposed_place(stack_b);
 	ndmax = get_ndmax(stack_b);
-	i = 1;
-	while (tmp->next != NULL)
+	i = 0;
+	while (i <= nb_instack)
 	{
-		if (tmp->suposed_place == max || tmp->suposed_place == ndmax)
+		if (tmp->suposed_place == max)
 			break ;
 		tmp = tmp->next;
 		i++;
 	}
-	if (i < nb_instack / 2)
+	if (i < (nb_instack / 2))
 		return (0);
 	else
 		return (1);
-
 }
 
 int	get_max_suposed_place(t_stack **stack)
@@ -71,4 +70,49 @@ int	get_ndmax(t_stack **stack)
 		tmp = tmp->next;
 	}
 	return (ndmax);
+}
+
+int	verif_in_order(t_stack *stack_a)
+{
+	t_stack	*temp;
+	t_stack	*temp2;
+
+	temp = stack_a;
+	temp2 = stack_a->next;
+	while (temp2)
+	{
+		if (temp->suposed_place >= temp2->suposed_place)
+			return (1);
+		temp = temp->next;
+		temp2 = temp2->next;
+	}
+	return (0);
+}
+
+int	way_to_rotate_presort(t_stack **stack_b, int max)
+{
+	t_stack	*tmp;
+	int		i;
+	int		nb_instack;
+	int		first;
+	int		last;
+
+	last = 0;
+	first = 0;
+	tmp = *stack_b;
+	nb_instack = get_last_index(*stack_b) - 1;
+	i = 0;
+	while (i <= nb_instack)
+	{
+		if (tmp->suposed_place <= max && first == 0)
+			first = i;
+		else if (tmp->suposed_place <= max && first > 0)
+			last = i;
+		tmp = tmp->next;
+		i++;
+	}
+	if (first < nb_instack - last + (nb_instack / 10))
+		return (1);
+	else
+		return (0);
 }
